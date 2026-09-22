38b3aba1b13d132762a707336b29e8a78e611b1a
shaikh-tech-gh <sahilshaikh0729@gmail.com>
2026-09-22 18:14:35 +0530

    Added Lenis

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,23 @@ import { useGSAP } from '@gsap/react';
 import Page3 from './components/Page3';
 import Page4 from './components/Page4';
 import Page5 from './components/Page5';
+import SmoothScroll from './components/SmoothScroll';
 
 const App = () => {
 
   return (
-    <div className='h-screen w-full'>
-      <Navbar />
-      <Page1 />
-      <Page2 />
-      <Page3 />
-      <Page4 />
-      <Page5 />
-    </div>
+    <>
+      <SmoothScroll />
+
+      <div className='h-screen w-full'>
+        <Navbar />
+        <Page1 />
+        <Page2 />
+        <Page3 />
+        <Page4 />
+        <Page5 />
+      </div>
+    </>
   )
 }
 
---
+++ src/components/SmoothScroll.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const SmoothScroll = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      syncTouch: false,
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const update = (time) => {
+      lenis.raf(time * 1000);
+    };
+
+    gsap.ticker.add(update);
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(update);
+      lenis.destroy();
+    };
+  }, []);
+
+  return null;
+};
+
+export default SmoothScroll;
